87eec54363372081ba6d6c770545cd6c5f6d5d75
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2025-05-04 15:42:33 +0530

    first commit

--- run-tests.sh
+++ run-tests.sh
@@ -6,13 +6,17 @@ docker-compose up -d
 # Wait for services to start
 sleep 10
 
-# Test without load balancing
+# Get container IPs
+BACKEND1_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' aws-load-balancer-nginx-backend1-1)
+LB_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' aws-load-balancer-nginx-lb-1)
+
+# Test without load balancing (direct to backend)
 echo "Testing without load balancing..."
-ab -n 1000 -c 100 http://nginx-backend1/ > results_no_lb.txt
+ab -n 1000 -c 100 http://$BACKEND1_IP/ > results_no_lb.txt
 
 # Test with load balancing
 echo "Testing with load balancing..."
-ab -n 1000 -c 100 http://nginx-lb/ > results_with_lb.txt
+ab -n 1000 -c 100 http://$LB_IP/ > results_with_lb.txt
 
 # Compare results
 echo "Results without LB:"
